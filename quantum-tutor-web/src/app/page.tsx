c6e74a68d2ca3da7ec5efcf5b8ee5124674fdd79
mohad-natheem <natheem2580@gmail.com>
2024-09-15 23:54:58 +0530

    Added google signin

--- quantum-tutor-web/src/app/page.tsx
+++ quantum-tutor-web/src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 import styles from './page.module.scss'
 import Image from 'next/image'
 import DashboardImage from '../../public/hero.png'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 
-const Dashboard = () => {
+const Dashboard =  () => {
+const session = useSession()
+const router = useRouter()
+console.log(`Login status of user ${session.status} and user email id ${session.data?.user?.email}`)
+
+if(session.status == "authenticated"){
+    router?.push("/app")
+}
   return (
     <Box className={styles.container}>
         <Box className={styles.item_left}>
@@ -18,8 +28,10 @@ const Dashboard = () => {
                 Chat to start, let's embark on a quest where every question is a step towards enlightenment
             </Typography>
             <Box className={styles.button_container}>
-            <Button sx={{background:'#53c28b', color:'white', padding:'10px 15px',border:'none',fontSize:'1.5',borderRadius:'20px'}} className={styles.button}>
-                Sign In
+            <Button onClick={async ()=>{
+                await signIn("google")
+                }} sx={{background:'#53c28b', color:'white', padding:'10px 15px',border:'none',fontSize:'1.5',borderRadius:'20px'}} className={styles.button}>
+                Sign In With Google
             </Button>
             </Box>
         </Box>
