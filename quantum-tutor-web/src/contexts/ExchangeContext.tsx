f30e59ac12292cdbe5f2f202a486e12bc37a1a0c
Mohamed Natheem <111576168+mohad-natheem@users.noreply.github.com>
2024-09-22 20:31:57 +0530

    Added code to fetch from the chat endpoint (#31)

--- quantum-tutor-web/src/contexts/ExchangeContext.tsx
+++ quantum-tutor-web/src/contexts/ExchangeContext.tsx
@@ -1,52 +1,87 @@
 "use client"
 import { createContext, useEffect, useState } from 'react';
 
+interface Message {
+  sender: string;
+  text: string;
+  chatId: string | null
+}
+
+interface Data {
+  message: Message;
+}
+
 interface ExchangeContextType {
-  data: any;
+  data: Data;
   isLoading: boolean;
 }
 
 const initialState: ExchangeContextType = {
   data: {
-    message: 'init',
+    message: {
+      sender: "user",
+      text: "Hii, hello0000?",
+      chatId: "chat_ca925b"
+    }
   },
-  isLoading: true,
-}
+  isLoading: false
+};
+
 const DataContext = createContext<ExchangeContextType>(initialState);
 
 function DataProvider({ children }: { children: React.ReactNode }) {
-  const [data, setData] = useState<any>({message: "init"});
+  const [data, setData] = useState<any>({});
   const [isLoading, setIsLoading] = useState(true);
+  const [userChatId, setUserChatId] = useState(null);
 
-  // const fetchData = async (body: any) => {
-  //   setIsLoading(true);
-  //   try {
-  //     const response = await fetch('https://your-api-endpoint', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ text }),
-  //     });
-  //     const data = await response.json();
-  //     setData(data);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+  const initialState: ExchangeContextType = {
+    data: {
+      message: {
+        sender: "user",
+        text: "Hii, hello0000?",
+        chatId: userChatId
+      }
+    },
+    isLoading: false
+  };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setTimeout(() => {
-        setData({ message: 'A data structure is a particular way of organizing data in a computer so that it can be used efficiently. They are essential for writing efficient algorithms. The choice of a data structure affects the performance of algorithms, particularly in terms of speed and memory use.' });
-        setIsLoading(false);
-      }, 100);
-    };
+useEffect(()=>{
+  const fetchData = async (body: any) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('http://127.0.0.1:8000/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      console.log("User request body")
+      console.log(body)
+      const data = await response.json();
+      setData(data);
+      data?.message.chatId && setUserChatId(data?.message.chatId)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  fetchData(initialState.data);
+  console.log("Bot Response");
+  console.log(data.message);
+},[])
 
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setTimeout(() => {
+  //       setData({ message: 'A data structure is a particular way of organizing data in a computer so that it can be used efficiently. They are essential for writing efficient algorithms. The choice of a data structure affects the performance of algorithms, particularly in terms of speed and memory use.' });
+  //       setIsLoading(false);
+  //     }, 100);
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
     <DataContext.Provider value={{data,isLoading}}>
