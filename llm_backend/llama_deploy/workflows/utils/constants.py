1dff5fa1f9336885677bbcc3f211da259abc8a42
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2024-09-30 01:42:03 +0530

    Renamed llm-backend and added dev server and prod server starting scripts

--- llm_backend/llama_deploy/workflows/utils/constants.py
+++
@@ -1,8 +0,0 @@
-from llama_index.core.llms import MessageRole
-
-roles = {
-    'user': MessageRole.USER, 
-    'system':MessageRole.SYSTEM, 
-    'bot': MessageRole.ASSISTANT, 
-    'tool': MessageRole.TOOL
-}
