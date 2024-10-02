28290d7537c26ff9d276d8467e30f05e74ef639b
mohad-natheem <natheem2580@gmail.com>
2024-10-02 19:43:42 +0530

    Did routing and content fix

--- quantum-tutor-web/src/components/ChatContainer/ChatContainer.tsx
+++ quantum-tutor-web/src/components/ChatContainer/ChatContainer.tsx
@@ -3,12 +3,21 @@ import React, { useContext, useEffect } from 'react';
 import { PromptContext } from '@/app/app/layout';
 import SingleExchange from '../Exchange/SingelExchange';
 import ChatHistory from './ChatHistory';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const ChatContainer = ({ chatSession }: { chatSession: any }) => {
   const dataContext = useContext(PromptContext);
+  const session = useSession();
+  const router = useRouter();
   console.log(dataContext, chatSession);
   const { data, isLoading, setData, currentModuleId } = dataContext;
 
+  if(session.status === "unauthenticated")
+  {
+    router.push("/")
+  }
+  
   useEffect(() => {
     let parsedSession = JSON.parse(chatSession?.value);
     if (parsedSession && data.length === 0) setData(parsedSession);
