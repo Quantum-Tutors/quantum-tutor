ab5371e8a6f28fc7ba79820b5e04f9e46069eb0f
mohad-natheem <natheem2580@gmail.com>
2024-10-04 01:07:49 +0530

    Content changes

--- quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
+++ quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
@@ -7,7 +7,7 @@ const MaintenanceBanner = () => {
         <img src="./maintenance.png" alt="maintenance" />
         <div className={styles.text_container}>
         <h1 className={styles.header}>APP UNDER MAINTENANCE</h1>
-        <h4 className={styles.subheader}>We are facing a technical issue and difficulties in deploying our AI modle in free tier.</h4>
+        <h4 className={styles.subheader}>We are facing a technical issue and difficulties in deploying our AI model in free tier.</h4>
         </div>
     </div>
   )
