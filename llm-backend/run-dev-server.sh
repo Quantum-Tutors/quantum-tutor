815175e9e2a97da5817277f107746e365dc9055d
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2024-10-04 16:07:35 +0530

    init commit

--- llm-backend/run-dev-server.sh
+++ llm-backend/run-dev-server.sh
@@ -3,16 +3,8 @@ start_server() {
   echo "Server started for $1"
 }
 
-pip_install(){
-  pip install -r "$1"
-}
+# start_server "llm_deploy.core_systems.main"
+# start_server "llm_deploy.workflows.tutor"
+# start_server "server.app"
 
-pip_install "./server/requirements.txt"
-pip_install "./llm_deploy/core_systems/requirements.txt"
-pip_install "./llm_deploy/workflows/tutor.py"
-
-start_server "llm_deploy.core_systems.main"
-start_server "llm_deploy.workflows.tutor"
-start_server "server.app"
-
-wait
+# wait
