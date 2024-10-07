44fc17714645d02354374ad3a6f0a6adfaf995d8
Hari hara sankar <hariharasankar.k.a@gmail.com>
2024-10-07 13:26:09 +0530

    Removal-maintenance-banner (#68)
    * (feat): Complete workflow and chat history and loading
    
    * (feat): ModuleId flow and some bugs
    
    * (feat): Remaining funtionalities, rag
    
    * Website Live - Removed Maintenance Banner
    
    * update banner info for rag

--- quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
+++ quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
@@ -4,13 +4,17 @@ import styles from '../../styles/MaintenanceBanner.module.scss';
 const MaintenanceBanner = () => {
   return (
     <div className={styles.container}>
-        <img src="./maintenance.png" alt="maintenance" />
-        <div className={styles.text_container}>
-        <h1 className={styles.header}>APP UNDER MAINTENANCE</h1>
-        <h4 className={styles.subheader}>We are facing a technical issue and difficulties in deploying our AI model in free tier. Meanwhile you could check out our chat history, model selection, signin features.</h4>
-        </div>
+      <img src='./maintenance.png' alt='maintenance' />
+      <div className={styles.text_container}>
+        <h1 className={styles.header}>RAG Feature is UNDER MAINTENANCE</h1>
+        <h4 className={styles.subheader}>
+          We've reached the storage limit in our DB, on Free tier. Hence our RAG
+          feature isn't working on the live website. Sorry for the
+          inconvenience.
+        </h4>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MaintenanceBanner
