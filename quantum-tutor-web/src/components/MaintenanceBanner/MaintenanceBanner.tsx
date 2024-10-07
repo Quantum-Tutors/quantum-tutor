85a9cd421dde1e2311656b5556dd1c98c3da68d5
hari <hariharasankar.k.a@gmail.com>
2024-10-07 14:27:48 +0530

    (merge): cobflict fix

--- quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
+++ quantum-tutor-web/src/components/MaintenanceBanner/MaintenanceBanner.tsx
@@ -4,13 +4,17 @@ import styles from '../../styles/MaintenanceBanner.module.scss';
 const MaintenanceBanner = () => {
   return (
     <div className={styles.container}>
-        <img src="./maintenance.png" alt="maintenance" />
-        <div className={styles.text_container}>
+      <img src='./maintenance.png' alt='maintenance' />
+      <div className={styles.text_container}>
         <h1 className={styles.header}>RAG Feature is UNDER MAINTENANCE</h1>
-        <h4 className={styles.subheader}>We've reached the storage limit in our DB, on Free tier. Hence our RAG feature isn't working on the live website. Sorry for the inconvenience.</h4>
-        </div>
+        <h4 className={styles.subheader}>
+          We've reached the storage limit in our DB, on Free tier. Hence our RAG
+          feature isn't working on the live website. Sorry for the
+          inconvenience.
+        </h4>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MaintenanceBanner
